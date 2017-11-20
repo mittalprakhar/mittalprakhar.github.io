06b0a96641cff149373536e7a02b211c50f42e9d
Prakhar Mittal <prakharmittal@Prakhars-Macintosh.local>
2017-11-20 23:42:32 +0530

    Switching to Materialize
    Drastic Changes

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {  
+    $(".button-collapse").sideNav({
+      menuWidth: 300,
+      draggable: true
+    });
+});
+
+ $(document).ready(function(){
+      $('.carousel').carousel();
+    });
