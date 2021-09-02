52eaa085dff6a5324c323f7d611d5e462f1218ed
Prakhar Mittal <mittalprakhar57@gmail.com>
2021-09-01 22:35:37 -0400

    New design base

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+function navbarExpand() {
+	document.querySelector('.nav-cross').classList.toggle('show');
+	document.querySelector('.nav-title').classList.toggle('show');
+	document.querySelector('.nav-items').classList.toggle('show');
+};
