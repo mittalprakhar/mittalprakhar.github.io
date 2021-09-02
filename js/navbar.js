ea9771a611ea0aa884e123136688558a5a84c5e5
Prakhar Mittal <mittalprakhar57@gmail.com>
2021-09-02 02:51:24 -0400

    Added cards

--- js/navbar.js
+++
@@ -1,5 +0,0 @@
-function navbarExpand() {
-	document.querySelector('.nav-cross').classList.toggle('show');
-	document.querySelector('.nav-title').classList.toggle('show');
-	document.querySelector('.nav-items').classList.toggle('show');
-};
